de26a4a25a4fe026258cbeb56a18e1d0b24d593c
rene <deshan-xiong@outlook.com>
2024-02-19 21:27:30 +0800

    lab2

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,15 +1,21 @@
 package kvsrv
 
-import "6.5840/labrpc"
+import (
+	"6.5840/labrpc"
+)
 import "crypto/rand"
 import "math/big"
 
+const MAXRETRY int = 10
 
 type Clerk struct {
 	server *labrpc.ClientEnd
 	// You will have to modify this struct.
+	cid int64
+	tid int
 }
 
+// nrand return a random non-negative int64
 func nrand() int64 {
 	max := big.NewInt(int64(1) << 62)
 	bigx, _ := rand.Int(rand.Reader, max)
@@ -21,6 +27,8 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.server = server
 	// You'll have to add code here.
+	ck.cid = nrand()
+	ck.tid = 0
 	return ck
 }
 
@@ -35,9 +43,14 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
-	// You will have to modify this function.
-	return ""
+	ck.tid = ck.tid + 1
+	args := GetArgs{key, ck.cid}
+	reply := GetReply{}
+	//log.Printf("Client %v Transaction %v Get %v from server\n", ck.cid, tid, key)
+	for ok := ck.server.Call("KVServer.Get", &args, &reply); !ok; ok = ck.server.Call("KVServer.Get", &args, &reply) {
+		//time.Sleep(time.Millisecond * 20)
+	}
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -49,8 +62,15 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
-	// You will have to modify this function.
-	return ""
+	tid := ck.tid
+	ck.tid = ck.tid + 1
+	args := PutAppendArgs{key, value, tid, ck.cid}
+	reply := PutAppendReply{}
+	//log.Printf("Client %v Transaction %v %v %v: %v to server\n", ck.cid, tid, op, key, value)
+	for ok := ck.server.Call("KVServer."+op, &args, &reply); !ok; ok = ck.server.Call("KVServer."+op, &args, &reply) {
+		//time.Sleep(time.Millisecond * 20)
+	}
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,78 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
-	// Your definitions here.
+	data  map[string]string
+	cache map[int64]struct {
+		history string
+		tid     int
+	}
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	key := args.Key
+	cid := args.CID
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if value, ok := kv.data[key]; ok {
+		reply.Value = value
+	} else {
+		reply.Value = ""
+	}
+	delete(kv.cache, cid)
+	//log.Printf("get %v: %v from memory\n", key, reply.Value)
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	key := args.Key
+	value := args.Value
+	cid := args.CID
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.data[key] = value
+	delete(kv.cache, cid)
+	//log.Printf("put %v: %v into memory\n", key, reply.Value)
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	key := args.Key
+	value := args.Value
+	cid := args.CID
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if history, ok := kv.cache[cid]; ok && history.tid == args.TID {
+		reply.Value = history.history
+	} else {
+		old, ok := kv.data[key]
+		if !ok {
+			old = ""
+		}
+		kv.data[key] = old + value
+		reply.Value = old
+
+		kv.cache[cid] = struct {
+			history string
+			tid     int
+		}{history: reply.Value, tid: args.TID}
+	}
+	//log.Printf("append %v: %v into memory\n", key, reply.Value)
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
-
-	// You may need initialization code here.
+	kv.data = make(map[string]string)
+	kv.cache = make(map[int64]struct {
+		history string
+		tid     int
+	})
 
 	return kv
 }
